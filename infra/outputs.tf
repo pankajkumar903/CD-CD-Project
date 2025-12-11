010242dff4497ed84b491ce143439e48b3c1e787
Pankaj Kumar <58365168+pankajkumar903@users.noreply.github.com>
2025-12-11 11:51:21 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,23 @@
-output "resource_group" {
-value = azurerm_resource_group.rg.name
-}
-
-output "acr_name" {
-value = azurerm_container_registry.acr.name
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
 output "aks_name" {
-value = azurerm_kubernetes_cluster.aks.name
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_kubelet_identity" {
+  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
+
+output "acr_name" {
+  value = var.acr_name
+}
+
+output "acr_id" {
+  value = local.acr_id
+}
+
+output "acr_login_server" {
+  value = local.acr_login_server
 }
