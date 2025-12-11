5ebd00f4a70b39c4fc89d62d0437b43b4b6e9c23
Pankaj Kumar <58365168+pankajkumar903@users.noreply.github.com>
2025-12-11 12:41:15 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,41 +1,34 @@
 variable "resource_group_name" {
-  description = "Name of the resource group to use or create"
-  type        = string
-  default     = "rg-devops-aks"
+  type    = string
+  default = "rg-devops-aks"
 }
 
 variable "location" {
-  description = "Azure region for resources"
-  type        = string
-  default     = "West US 2"
+  type    = string
+  default = "West US 2"
 }
 
 variable "acr_name" {
-  description = "Azure Container Registry name (must be globally unique)"
-  type        = string
-  default     = "devopsacr00123"
+  type    = string
+  default = "devopsacr00123"
 }
 
 variable "aks_name" {
-  description = "Name of the AKS cluster"
-  type        = string
-  default     = "devops-aks"
+  type    = string
+  default = "devops-aks"
 }
 
 variable "agent_pool_name" {
-  description = "AKS default node pool name"
-  type        = string
-  default     = "nodepool1"
+  type    = string
+  default = "nodepool1"
 }
 
 variable "agent_node_count" {
-  description = "Number of nodes in AKS node pool"
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "agent_vm_size" {
-  description = "VM size for AKS nodes"
-  type        = string
-  default     = "Standard_B2s_v2"
+  type    = string
+  default = "Standard_B2s_v2"
 }
